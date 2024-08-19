703357eb42c6df95462547f2e931cdfde786ed7e
debugsTech <sal@debugs.tech>
2024-08-19 11:51:33 -0700

    use helper function for 404 instead

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,8 @@
+from gc import get_objects
 
 from django.http import HttpResponse
-from django.shortcuts import render
-from django.http import Http404
+from django.shortcuts import get_object_or_404, render
+
 
 from .models import Question
 
@@ -22,10 +23,7 @@ def results(request, question_id):
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
 
-def detail(requst, question_id):
-    try:
-        question = Question.object.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(requst, "polls/details.html", {"question": question})
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
