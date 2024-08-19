88f8bb9512d9639ab55c4fd86a62071bea9f548e
debugsTech <sal@debugs.tech>
2024-08-19 00:41:05 -0700

    fixed typo in views.py

--- polls/views.py
+++ polls/views.py
@@ -3,8 +3,8 @@ from django.http import HttpResponse
 from .models import Question
 
 def index(request):
-   latest_question_list = Question.objects.orderby("-pub_date")[:5]
-   output = ", ".join([q.question.question_text for q in latest_question_list])
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    output = ", ".join([q.question_text for q in latest_question_list])
     return HttpResponse(output)
 
 def detail(request, question_id):
